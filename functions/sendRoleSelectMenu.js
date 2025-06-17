28c3a035eea802ce39ab5f7189fe8b688b169396
camp_.ksk <camp131249@gmail.com>
2025-06-17 18:13:01 +0700

    Reduce duplicate code "rolemenu"

--- functions/sendRoleSelectMenu.js
+++ functions/sendRoleSelectMenu.js
@@ -1,44 +1,7 @@
-const { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
-const { roleOptions } = require('../data/roleOptions');
+const { rolemenu } = require("../data/rolemenu");
 
 module.exports = async function sendRoleSelectMenu(channel) {
-  const embed = new EmbedBuilder()
-    .setTitle('🎭 รับยศด้วยตัวคุณเอง!')
-    .setDescription('เลือกรับยศตามหมวดหมู่ด้านล่าง')
-    .setColor('#FF9B45');
-
-  const genderSelect = new StringSelectMenuBuilder()
-    .setCustomId('gender_select')
-    .setPlaceholder('เลือกเพศของคุณ')
-    .setMinValues(0)
-    .setMaxValues(1)
-    .addOptions(roleOptions.gender);
-
-  const colorSelect = new StringSelectMenuBuilder()
-    .setCustomId('color_select')
-    .setPlaceholder('เลือกสีประจำตัว')
-    .setMinValues(0)
-    .setMaxValues(1)
-    .addOptions(roleOptions.color);
-
-  const gameSelect = new StringSelectMenuBuilder()
-    .setCustomId('game_select')
-    .setPlaceholder('เลือกเกมที่คุณเล่น')
-    .setMinValues(0)
-    .setMaxValues(6)
-    .addOptions(roleOptions.game);
-
-  const interestedSelect = new StringSelectMenuBuilder()
-    .setCustomId('interested_select')
-    .setPlaceholder('เลือกความสนใจ')
-    .setMinValues(0)
-    .setMaxValues(11)
-    .addOptions(roleOptions.interested);
-
-  const row1 = new ActionRowBuilder().addComponents(genderSelect);
-  const row2 = new ActionRowBuilder().addComponents(colorSelect);
-  const row3 = new ActionRowBuilder().addComponents(gameSelect);
-  const row4 = new ActionRowBuilder().addComponents(interestedSelect);
+  const { embed, row1, row2, row3, row4 } = rolemenu();
 
   await channel.send({
     embeds: [embed],
